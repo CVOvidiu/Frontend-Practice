0b2c357cc4c48af8fe964fefa9e4722fbadde7eb
CVOvidiu <cvovidiu2002@gmail.com>
2022-12-27 02:27:45 +0200

    Update

---
+++ interactive-rating-component-main/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      overpass: ["Overpass"],
+    },
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
